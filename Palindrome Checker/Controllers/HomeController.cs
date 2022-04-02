95e4e36f15fa0d64cdb4d1a9d06720162f742079
meganlynn21 <meganlkeyes0203@gmail.com>
2022-04-02 15:24:33 -0400

    Created a Palindrome Checker in C# using ASP.NET MVC. Created a web link to click on Palindrome page. Created content to ask for input.

---
+++ Palindrome Checker/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Palindrome_Checker.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Palindrome_Checker.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult Reverse()
+        {
+            Palindrome model = new();
+            return View(model);
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Palindrome Checker/Models/Palindrome.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Palindrome_Checker.Models
+{
+    public class Palindrome
+    {
+        public string InputWord { get; set; }
+
+        public string RevWord { get; set; }
+
+        public bool IsPalindrome { get; set; }
+
+        public string Message { get; set; }
+    }
+}
